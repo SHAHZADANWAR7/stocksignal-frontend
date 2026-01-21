4b922337bf757db8a4484f30be061b2bbe355cfc
Shahzad Anwar <dr.shahzadanwar40@yahoo.com>
2026-01-21 10:15:07 +0000

    Add awsClient export to utils barrel

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,2 @@
-/**
- * Creates a page URL for navigation
- * @param {string} pageName - The page name (e.g., "Dashboard", "Holdings")
- * @param {string} params - Optional URL parameters (e.g., "?id=123&sort=asc")
- * @returns {string} - The page URL path
- */
-export const createPageUrl = (pageName, params = '') => {
-  return `/${pageName}${params}`;
-};
+export { createPageUrl } from "./index.js";
+export * from "./awsClient.js";
