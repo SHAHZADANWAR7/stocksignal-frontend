ce5b841c2c5060bc678e521ee6be4914444b7f12
Shahzad Anwar <dr.shahzadanwar40@yahoo.com>
2026-01-23 07:45:28 +0000

    Fix: Force Vite to bundle globals.css with ?inline

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './globals.css'
+import globals from './globals.css?inline'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
