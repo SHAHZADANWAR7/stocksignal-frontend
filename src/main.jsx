0e6c1e146bf4acae4f907e57e802ae6f62ca23d1
Shahzad Anwar <dr.shahzadanwar40@yahoo.com>
2026-01-24 10:30:37 +0000

    Fix: Remove duplicate globals.css import

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import './globals.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './globals.css'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
