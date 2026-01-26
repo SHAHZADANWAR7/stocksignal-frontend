b2abf0852a7995c453e99d7d2d1e511445c85ae4
Shahzad Anwar <dr.shahzadanwar40@yahoo.com>
2026-01-26 07:29:05 +0000

    Add debug logging for Cognito env vars

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Amplify } from 'aws-amplify'
 
+// DEBUG: Check if env vars are loaded
+console.log('Env vars:', {
+  poolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+  clientId: import.meta.env.VITE_COGNITO_APP_CLIENT_ID,
+  region: import.meta.env.VITE_COGNITO_REGION,
+})
+
 // Configure AWS Amplify with Cognito
 Amplify.configure({
   Auth: {
