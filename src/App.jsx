8fa516010de0a2a5eec4a7f572e0d3cee927c1d2
Shahzad Anwar <dr.shahzadanwar40@yahoo.com>
2026-01-27 09:57:39 +0000

    fix: add Login route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Companies from './pages/Companies'
 import Analysis from './pages/Analysis'
@@ -32,6 +33,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          
+          <Route path="/login" element={<Login />} />
           
           <Route path="/dashboard" element={<Dashboard />} />
           
