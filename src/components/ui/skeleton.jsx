56a5522dfb5f0c38a80df52b6a954c591a54c2ef
SHAHZADANWAR7 <dr.shahzadanwar40@yahoo.com>
2026-02-03 06:55:58 +0000

    chore(ui): add Skeleton component at src/components/ui/skeleton.jsx

---
+++ src/components/ui/skeleton.jsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+function Skeleton({
+  className,
+  ...props
+}) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-slate-200", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
